7ce36738b8fdd1c274e355c9c7f3d65db02d19ce
RioKaroba <karobario051@gmail.com>
2026-03-14 22:15:00 +0700

    Ai

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class EmailVerificationPromptController extends Controller
+class VerifyEmailController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('auth.verify-email');
+        return redirect('/');
     }
 }
---
+++ resources/home.blade.php
@@ -0,0 +1,2 @@
+<h1>Home Page</h1>
+<p>Toko Ulos Laravel berjalan</p>
