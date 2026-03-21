e60b352c1b334739c3d2b68cb4a74c2d5d42a486
RioKaroba <karobario051@gmail.com>
2026-03-21 23:04:17 +0700

    update DatabaseSeeder call ProdukSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,20 +10,15 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Admin Elizabeth',
             'email' => 'admin@elizabethulos.com',
         ]);
 
         $this->call([
-            KategoriSeeder::class,
+            ProdukSeeder::class,
         ]);
     }
 }
