896b4c4a9312db63ca68ab4a92ce5fbc35b07218
RioKaroba <karobario051@gmail.com>
2026-03-21 23:15:56 +0700

    fix DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     public function run(): void
     {
-        User::factory()->create([
+        // Buat user admin
+        DB::table('users')->insertOrIgnore([
             'name' => 'Admin Elizabeth',
             'email' => 'admin@elizabethulos.com',
+            'password' => Hash::make('password123'),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         $this->call([
